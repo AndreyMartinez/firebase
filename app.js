f24caed16b223ceffb002839a04d481ad61cdb1f
raphael martinez <root@localhost.localdomain>
2018-09-29 14:37:08 -0500

    work time real firebase

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 firebase.initializeApp({
-    apiKey: 'AIzaSyBEtuno5v_-37JrV6EWPczjgHF0bn0J4Rc',
-    authDomain: 'ionic1-42d1d.firebaseapp.com',
-    projectId: 'ionic1-42d1d'
-});
 
+    apiKey: "AIzaSyBEtuno5v_-37JrV6EWPczjgHF0bn0J4Rc",
+    authDomain: "ionic1-42d1d.firebaseapp.com",
+    databaseURL: "https://ionic1-42d1d.firebaseio.com",
+    projectId: "ionic1-42d1d",
+    storageBucket: "ionic1-42d1d.appspot.com",
+    messagingSenderId: "91962131300"
+
+});
+var firstTitle = document.getElementsByClassName('topContent')
+var conection =  firebase.database().ref().child('text')
+conection.on('value',param => firstTitle[0].innerText = JSON.stringify(param.val().edad))
 // Initialize Cloud Firestore through Firebase
-var db = firebase.firestore();
+// var db = firebase.firestore();
 
 // db.collection("users").add({
 //     first: "Elene",
@@ -19,12 +26,12 @@ var db = firebase.firestore();
 //     .catch(function (error) {
 //         console.error("Error adding document: ", error);
 //     });
-db.collection("users").where("born","==",1940).get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(doc)
-        console.log(`${doc.id} => ${doc.data()}`);
-    });
-});
+// db.collection("users").where("born","==",1940).get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         console.log(doc)
+//         console.log(`${doc.id} => ${doc.data()}`);
+//     });
+// });
 
     // db.collection("users").get().then((querySnapshot) => {
     //     querySnapshot.forEach((doc) => {
